3dfdbd31d389fcb89936bc732d41d80a22257209
Nguyen Thanh <nguyenthanh@MacBook-Pro.local>
2020-06-21 23:36:58 +0700

    .

--- 023_serving-files/02_serving/04_FileServer/02/main.go
+++ 023_serving-files/02_serving/04_FileServer/02/main.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", dog)
+	//Hàm này sẽ loaị bỏ phần đường dẫn /resource trong câu request và export files trong thư muc ./assets
 	http.Handle("/resources/", http.StripPrefix("/resources", http.FileServer(http.Dir("./assets"))))
 	http.ListenAndServe(":8080", nil)
 }
